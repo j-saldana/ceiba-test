9d8944ce65aa8f8cea468bd52dbc6e7eff268495
j-saldana <juan.saldana@correounivalle.edu.co>
2020-10-17 14:45:32 -0500

    Created SharedPreferences for dave data
     - Created a SharedPreference on MainActivity and PostActivity
     - Conditionals for load data or consume web services
     - Added progressbar when consume the services

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.java
@@ -1,12 +1,23 @@
 package co.com.ceiba.mobile.pruebadeingreso.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Objects;
 
 import co.com.ceiba.mobile.pruebadeingreso.R;
 import co.com.ceiba.mobile.pruebadeingreso.model.User;
@@ -18,16 +29,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends Activity {
+    private SharedPreferences preferences;
+    private Gson gson;
     private Api jsonApi;
     private RecyclerView recycler;
-    private UsersAdapter usersAdapter;
     private List<User> users;
+    private Dialog progress;
 
-
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init sharedPreferences
+        preferences = getSharedPreferences("DATA", MODE_PRIVATE);
+
+        //Init Gson to save objects
+        gson = new Gson();
 
         //Init Api Interface
         jsonApi = RetrofitClient.getRetrofitInstance().create(Api.class);
@@ -36,12 +55,31 @@ public class MainActivity extends Activity {
         recycler = findViewById(R.id.recyclerViewSearchResults);
         recycler.setLayoutManager(new LinearLayoutManager(this));
 
+        //Init progress
+        progress = new Dialog(this);
+        progress.setContentView(R.layout.progress);
+        progress.setCancelable(false);
+        Objects.requireNonNull(progress.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        getUsers();
+        String json = preferences.getString("users", null);
+        if (json != null){
+            Type type = new TypeToken<List<User>>(){}.getType();
+            users = gson.fromJson(json, type);
+            printRecycler();
+        }else {
+            progress.show();
+            getUsers();
+        }
+    }
+
+    private void printRecycler() {
+        //Pass Adapter to Recycler
+        UsersAdapter usersAdapter = new UsersAdapter(MainActivity.this, users);
+        recycler.setAdapter(usersAdapter);
     }
 
     private void getUsers(){
@@ -51,23 +89,26 @@ public class MainActivity extends Activity {
             @Override
             public void onResponse(Call<List<User>> call, Response<List<User>> response) {
                 if (!response.isSuccessful()){
-                    Log.d("ERROR ", "onResponse: "+response.code());
+                    Log.d("ERROR ON USERS ", "onResponse: "+response.code());
                 }else {
                     users = response.body();
-                    Log.d("CORRECT ", "onResponse: "+ response.code());
+                    Log.d("CORRECT ON USERS ", "onResponse: "+ response.code());
 
-                    //Pass Adapter to Recycler
-                    usersAdapter = new UsersAdapter(MainActivity.this, users);
-                    recycler.setAdapter(usersAdapter);
+                    //Save data on sharedPreferences
+                    String json = gson.toJson(users);
+                    preferences.edit().putString("users", json).apply();
+
+                    printRecycler();
                 }
+                progress.dismiss();
             }
 
             @Override
             public void onFailure(Call<List<User>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failure petition on Users", Toast.LENGTH_LONG).show();
                 Log.d("FAILURE", "onFailure: "+t.getMessage());
+                progress.dismiss();
             }
         });
     }
-
-
 }
--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/PostActivity.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/PostActivity.java
@@ -1,15 +1,27 @@
 package co.com.ceiba.mobile.pruebadeingreso.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import co.com.ceiba.mobile.pruebadeingreso.R;
 import co.com.ceiba.mobile.pruebadeingreso.model.Post;
@@ -21,18 +33,26 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class PostActivity extends Activity {
+    private SharedPreferences preferences;
+    private Gson gson;
     private Api jsonApi;
     private RecyclerView recycler;
-    private PostsAdapter postsAdapter;
     private List<Post> posts;
-
+    private Dialog progress;
     private TextView userName, userPhone,userEmail;
 
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        //Init sharedPreferences
+        preferences = getSharedPreferences("DATA", MODE_PRIVATE);
+
+        //Init Gson to save objects
+        gson = new Gson();
 
 
         //Init Api Interface
@@ -47,6 +67,12 @@ public class PostActivity extends Activity {
         userPhone = findViewById(R.id.phone);
         userEmail = findViewById(R.id.email);
 
+        //Init progress
+        progress = new Dialog(this);
+        progress.setContentView(R.layout.progress);
+        progress.setCancelable(false);
+        Objects.requireNonNull(progress.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
     }
 
     @Override
@@ -54,34 +80,60 @@ public class PostActivity extends Activity {
         super.onStart();
         Bundle extras = getIntent().getExtras();
         if (extras != null){
+            String id = String.valueOf(extras.getInt("id"));
             userName.setText(extras.getString("name"));
             userPhone.setText(extras.getString("phone"));
             userEmail.setText(extras.getString("email"));
-            getPosts(String.valueOf(extras.getInt("id")));
+
+            String json = preferences.getString("posts/"+id, null);
+            if (json != null){
+                Type type = new TypeToken<List<Post>>(){}.getType();
+                posts = gson.fromJson(json, type);
+                printRecycler();
+            }else {
+                progress.show();
+                getPosts(id);
+            }
+        }else {
+            Toast.makeText(this,"An error occurred passing activity", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, MainActivity.class));
         }
     }
 
-    private void getPosts(String userId){
+    private void printRecycler() {
+        //Pass Adapter to Recycler
+        PostsAdapter postsAdapter = new PostsAdapter(PostActivity.this, posts);
+        recycler.setAdapter(postsAdapter);
+    }
+
+    private void getPosts(final String userId){
         Map<String, String> params = new HashMap<>();
         params.put("userId", userId);
-        Call<List<Post>> postsCall = jsonApi.getPosts(params);
+        final Call<List<Post>> postsCall = jsonApi.getPosts(params);
 
         postsCall.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
                 if (!response.isSuccessful()){
-                    Log.d("ERROR ", "onResponse: "+response.code());
+                    Log.d("ERROR ON POSTS ", "onResponse: "+response.code());
                 }else {
                     posts = response.body();
-                    Log.d("CORRECT ", "onResponse: "+ response.code());
-                    postsAdapter = new PostsAdapter(PostActivity.this, posts);
-                    recycler.setAdapter(postsAdapter);
+                    Log.d("CORRECT ON POSTS ", "onResponse: "+ response.code());
+
+                    //Save data on sharedPreferences
+                    String json = gson.toJson(posts);
+                    preferences.edit().putString("posts/"+userId, json).apply();
+
+                    printRecycler();
                 }
+                progress.dismiss();
             }
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
+                Toast.makeText(PostActivity.this, "Failure petition on Posts", Toast.LENGTH_LONG).show();
                 Log.d("FAILURE", "onFailure: "+t.getMessage());
+                progress.dismiss();
             }
         });
     }
