6a492b833a8fee288d9501ae919634c4d7de7ad0
j-saldana <juan.saldana@correounivalle.edu.co>
2020-10-17 22:15:56 -0500

    Created filter for Users
     - Added function for filter users and show empty view if the filter results is 0

--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/MainActivity.java
@@ -9,7 +9,15 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -33,6 +41,7 @@ public class MainActivity extends Activity {
     private Gson gson;
     private Api jsonApi;
     private RecyclerView recycler;
+    private UsersAdapter usersAdapter;
     private List<User> users;
     private Dialog progress;
 
@@ -60,6 +69,48 @@ public class MainActivity extends Activity {
         progress.setContentView(R.layout.progress);
         progress.setCancelable(false);
         Objects.requireNonNull(progress.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+        //Inflate empty view on activity with gravity center and invisible
+        RelativeLayout content = findViewById(R.id.content);
+        final LayoutInflater inflater = LayoutInflater.from(this);
+        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        params.addRule(Gravity.CENTER);
+        @SuppressLint("InflateParams") final View view = inflater.inflate(R.layout.empty_view, null);
+        view.setVisibility(View.INVISIBLE);
+        content.addView(view, params);
+
+        //Init editText and put on text change listener
+        EditText editText = findViewById(R.id.editTextSearch);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(final CharSequence charSequence, int i, int i1, int i2) {
+                usersAdapter.getFilter().filter(charSequence, new Filter.FilterListener() {
+                    @Override
+                    public void onFilterComplete(int i) {
+                        if (charSequence.toString().length() == 0){
+                            view.setVisibility(View.INVISIBLE);
+                        }else {
+                            if (usersAdapter.getItemCount() > 0){
+                                view.setVisibility(View.INVISIBLE);
+                            }else {
+                                view.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     @Override
@@ -78,7 +129,7 @@ public class MainActivity extends Activity {
 
     private void printRecycler() {
         //Pass Adapter to Recycler
-        UsersAdapter usersAdapter = new UsersAdapter(MainActivity.this, users);
+        usersAdapter = new UsersAdapter(MainActivity.this, users);
         recycler.setAdapter(usersAdapter);
     }
 
--- app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/adapters/users/UsersAdapter.java
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/view/adapters/users/UsersAdapter.java
@@ -3,27 +3,31 @@ package co.com.ceiba.mobile.pruebadeingreso.view.adapters.users;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
-import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import co.com.ceiba.mobile.pruebadeingreso.R;
 import co.com.ceiba.mobile.pruebadeingreso.model.User;
 import co.com.ceiba.mobile.pruebadeingreso.view.PostActivity;
 
-public class UsersAdapter extends RecyclerView.Adapter<UsersHolder> {
+public class UsersAdapter extends RecyclerView.Adapter<UsersHolder> implements Filterable {
     Context context;
     List<User> userList;
+    List<User> userListFull;
+
 
     public UsersAdapter(Context context, List<User> userList) {
         this.context = context;
         this.userList = userList;
+        userListFull = new ArrayList<>(userList);
     }
 
     @NonNull
@@ -60,4 +64,37 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersHolder> {
     public int getItemCount() {
         return userList.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return usersFilter;
+    }
+
+    private final Filter usersFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<User> filteredList = new ArrayList<>();
+            if (charSequence == null || charSequence.length() == 0){
+                filteredList.addAll(userListFull);
+            }else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+                for (User user: userListFull){
+                    if (user.getName().toLowerCase().contains(filterPattern)){
+                        filteredList.add(user);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            userList.clear();
+            userList.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 }
